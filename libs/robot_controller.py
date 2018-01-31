8f39d3f75b14d6310b1fe8966acc0cb47701cac8
shig <shig@rose-hulman.edu>
2018-01-31 12:37:13 -0500

    Robot controller DONE

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -26,6 +26,7 @@ class Snatch3r(object):
         self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
         assert self.left_motor.connected
         assert self.right_motor.connected
+
     def drive_inches(self, inches_target, speed_deg_per_second):
         degrees_per_inch = 90
         motor_turns_needed_in_degrees = inches_target * degrees_per_inch
@@ -33,14 +34,19 @@ class Snatch3r(object):
         self.right_motor.run_to_rel_pos(position_sp=motor_turns_needed_in_degrees,speed_sp=speed_deg_per_second)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+
     def turn_degrees(self, degrees_to_turn, turn_speed_sp):
+
         if degrees_to_turn > 0:
-            self.right_motor.run_to_rel_pos(position_sp= degrees_to_turn, speed_sp =turn_speed_sp )
-            self.left_motor.run_to_rel_pos(position_sp= degrees_to_turn, speed_sp =-turn_speed_sp )
-            self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+
+            self.right_motor.run_to_rel_pos(position_sp=degrees_to_turn, speed_sp =turn_speed_sp )
+            self.left_motor.run_to_rel_pos(position_sp=-degrees_to_turn, speed_sp =turn_speed_sp )
             self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+            self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+
         if degrees_to_turn < 0:
-            self.right_motor.run_to_rel_pos(position_sp=degrees_to_turn, speed_sp=-turn_speed_sp)
+            self.right_motor.run_to_rel_pos(position_sp=-degrees_to_turn, speed_sp=turn_speed_sp)
             self.left_motor.run_to_rel_pos(position_sp=degrees_to_turn, speed_sp=turn_speed_sp)
-            self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
             self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+            self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+
