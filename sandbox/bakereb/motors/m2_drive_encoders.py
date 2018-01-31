f1e31c04df8f6db15a8cc0339479708b5270e9a6
bakereb <bakereb@rose-hulman.edu>
2018-01-31 11:26:18 -0500

    Finished M2

--- sandbox/bakereb/motors/m2_drive_encoders.py
+++ sandbox/bakereb/motors/m2_drive_encoders.py
@@ -31,9 +31,9 @@ import time
 #        -- speed_sp
 #        -- stop_action
 
-# TODO: 5. Make sure the beep happens AFTER the motors stop.  Use the wait_while command to block code execution.
+# DONE: 5. Make sure the beep happens AFTER the motors stop.  Use the wait_while command to block code execution.
 
-# TODO: 6. Formally test your work. When you think you have the problem complete run these tests:
+# DONE: 6. Formally test your work. When you think you have the problem complete run these tests:
 #   200 dps 24 inches (make sure it drives within 2 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 2 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 2 inches of the target distance)
@@ -42,7 +42,7 @@ import time
 #   400 dps -36 inches (make sure it drives within 3 inches of the target distance)
 # Add more tests as you see fit.  Ideally you should be +/- 10% of the target goal this time.
 
-# TODO: 7. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+# DONE: 7. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
 #
 # Observations you should make, run_to_rel_pos is easier to use since it uses encoders that are independent of speed.
 
@@ -65,15 +65,16 @@ def main():
         speed_sp = int(input("Enter a speed for the motor (0 to 900 dps):"))
         if speed_sp == 0:
             break
-        distance = int(input("Enter a distance"))
+        inches_target = int(input("Enter a distance"))
         if speed_sp == 0:
             break
-        time_s = distance/(speed_sp*115/10000)
-        left_motor.run_forever(speed_sp=speed_sp)
-        right_motor.run_forever(speed_sp=speed_sp)
-        time.sleep(time_s)
-        left_motor.stop(stop_action="brake")
-        right_motor.stop(stop_action="brake")
+
+        degrees_per_inch = 90
+        motor_turns_needed_in_degrees = inches_target * degrees_per_inch
+        left_motor.run_to_rel_pos(position_sp = motor_turns_needed_in_degrees, speed_sp = speed_sp)
+        right_motor.run_to_rel_pos(position_sp = motor_turns_needed_in_degrees, speed_sp = speed_sp)
+        left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        right_motor.wait_while(ev3.Motor.STATE_RUNNING)
         ev3.Sound.beep().wait()
 
     print("Goodbye!")
