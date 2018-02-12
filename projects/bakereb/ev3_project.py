ba489178445c3c64ee42a3ddead3bcba5350a5f8
bakereb <bakereb@rose-hulman.edu>
2018-02-12 11:12:45 -0500

    Started Project

---
+++ projects/bakereb/ev3_project.py
@@ -0,0 +1,10 @@
+"""
+    The goal of this project will be to design a human-robot interaction game in which the human attempts to beat the
+    robot at doing some task. The human will use the GUI and/or physical interaction input commands to the robot and
+    the robot will respond in a unique way.
+
+    The theme is Terminator. The robot (i.e. the terminator) is trying to capture John Connor (i.e. the Beacon), and
+    your job is to try and prevent this from happening by hacking into the Terminator's mainframe and drive it away
+    using keyboard inputs. Once hacked, you will have a limited amount of time to try and drive the robot to cross a
+    line of a certain color, which will activate the EMP and deactivate the 
+"""
